cd0af2309a651482353e539e661910cfcce3ec31
ASValenciaV <143913026+ASValenciaV@users.noreply.github.com>
2023-09-02 20:36:00 -0500

    Create ejercicio4.java

---
+++ ejercicio4.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //ejercicio 4
+        int numA = 5;
+        int numB = -3;
+
+        if ((numA > 0 && numB > 0) || (numA < 0 && numB < 0)) {
+            System.out.println("true");
+        }
+    }
+}
+//Respuesta: No imprime nada
