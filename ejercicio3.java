dd7ae61aa48e18ddc8c93ab9f5ee2b14e5a93761
ASValenciaV <143913026+ASValenciaV@users.noreply.github.com>
2023-09-02 20:33:45 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //ejericio 3
+        int numA = 5;
+        int numB = 3;
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        }
+    }
+}
+//Respuesta: true
