ae7135760e1da48182577cc6b38dbe9f66ed8e99
ASValenciaV <143913026+ASValenciaV@users.noreply.github.com>
2023-09-02 20:29:27 -0500

    Create ejercicio2.java

---
+++ ejercicio2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //ejercicio 2
+        int num = 5;
+        if (num % 2 != 0 && num > 0) {
+            System.out.println("true");
+        }
+    }
+}
